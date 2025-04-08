2d07ab4ca27c26ab0bb6a4d882122fae67ac2b39
HowardPeter <phuocnt1611@gmail.com>
2025-04-08 18:34:54 +0700

    fix sonar 2 bugs

--- components/thinking.tsx
+++ components/thinking.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import LottieView from 'lottie-react-native'
 import { animations } from '@/constants'
 
-export default function Thinking({ size }: { size: number }) {
+export default function Thinking({ size }: Readonly<{ size: number }>) {
     return (
         <View style={{ height: size, aspectRatio: 1 }}>
             <LottieView style={{ flex: 1 }} source={animations.thinking} autoPlay loop />
